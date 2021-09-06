38365f88ed7aaab3602189fc47a08888fbeed142
Paula Leiva <88547780+PaulaLeiva@users.noreply.github.com>
2021-09-06 14:43:24 -0500

    Add files via upload

---
+++ Corte#1/Actividad #3/Problema_8/Program.cs
@@ -0,0 +1,61 @@
+﻿/*
+Create by: Maria Camila Morales Galeano, 
+           Maria Paula Leiva Agudelo,
+           Juan David Ropero Prieto,
+           John Fredy Marciales Morales.
+Group: 239-2A
+Shift: day
+*/
+
+/*
+Problema 8:
+Realizar un programa que pida dos números y presente por pantalla la ecuación de segundo grado que tiene por 
+soluciones estos dos números. Ejemplo: 5 y -3, la ecuación sería (x-5) (x+3) = x2 -2x-15, los coeficientes son 1, -2 
+y -15. El programa permitirá repetir esta operación tantas veces como el usuario quiera, introduciendo números 
+diferentes, hasta que decida terminar el programa (por ejemplo pulsando ‘S’).
+*/
+
+using System;
+
+namespace Problema_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero1;
+            int numero2 = 0;
+            string signo = "";
+            string signo2 = "";
+            char whileLoop = 'p';
+            
+            while(whileLoop != 'S'){
+
+            Console.Write("\n\nIngrese un número: ");
+            numero1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Ingrese un número: ");
+            numero2 = Convert.ToInt32(Console.ReadLine());
+            int form = numero1 + numero2;
+            int form2 = numero1 * numero2;
+            
+            if(form > 0){
+                signo = "+";
+            }else{
+                signo = "";
+            }
+            
+            if(form2 > 0){
+                signo2 = "+";
+            }else{
+                signo2 = "";
+            }
+
+            Console.WriteLine($"->La ecuación de segundo grado es: x{1*2} {signo} {form}x {signo2} {form2}");
+
+            Console.Write("¿Desea terminar el programa? S/N: ");
+            whileLoop = Console.ReadKey().KeyChar;
+
+            }
+        }
+    }
+}
---
+++ Corte#1/Actividad #3/Problema_9/Program.cs
@@ -0,0 +1,46 @@
+﻿/*
+Create by: Maria Camila Morales Galeano, 
+           Maria Paula Leiva Agudelo,
+           Juan David Ropero Prieto,
+           John Fredy Marciales Morales.
+Group: 239-2A
+Shift: day
+*/
+
+
+using System;
+
+namespace Problema_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lado1;
+            int lado2;
+            int lado3;
+
+            Console.Write("\nIngrese el lado 1 del triangulo: ");
+            lado1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Ingrese el lado 2 del triangulo: ");
+            lado2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Ingrese el lado 3 del triangulo: ");
+            lado3 = Convert.ToInt32(Console.ReadLine());
+
+
+            if((lado1 + lado2) > lado3){
+                if(lado1 == lado2 && lado1 == lado3){
+                    Console.WriteLine("El triángulo es Equilátero");
+                }else if(lado1 == lado2 || lado1 == lado3 || lado3 == lado2){
+                    Console.WriteLine("El triángulo es Isósceles");
+                }else if(lado1 != lado2 && lado2 != lado3){
+                    Console.WriteLine("El triángulo es Escaleno");
+                }
+                
+            }
+             Console.WriteLine("");
+        }
+    }
+}
