3f603e454fd37103c6d6958ca079b91273c78aa9
Paula Leiva <88547780+PaulaLeiva@users.noreply.github.com>
2021-09-06 14:42:00 -0500

    Add files via upload

---
+++ Corte#1/Actividad #3/Problema_6/Problema 6.cs
@@ -0,0 +1,38 @@
+﻿using System;
+//Authors:María Camila Morales Galeano,María Paula Leiva Agudelo,Jhon Fredy Marciales Morales,Juandavid Ropero Prieto.
+//Working day: Morning
+//Group:239-2ª
+
+namespace Problema_6
+{
+    class Problema_6
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            string dato;
+            
+            Console.WriteLine(" Por favor ingrese un número par entre 100 y 200: ");
+            dato=Console.ReadLine();
+            n = Convert.ToInt32(dato);
+
+            if(n<100 || n>200){
+                Console.WriteLine("El numero no está entre 100 y 200");
+                return;
+            }
+
+            if(n % 2 != 0){
+                Console.WriteLine("El numero es impar");
+                return;
+            }
+
+            for(int i=n;i<=200;i++){
+                if(i % 2 == 0){
+                    Console.WriteLine(i);
+                }
+                
+            }
+            
+        }
+    }
+}
---
+++ Corte#1/Actividad #3/Problema_7/Program.cs
@@ -0,0 +1,66 @@
+﻿/*
+Create by: Maria Camila Morales Galeano, 
+           Maria Paula Leiva Agudelo,
+           Juan David Ropero Prieto,
+           John Fredy Marciales Morales.
+Group: 239-2A
+Shift: day
+*/
+
+/*
+Problema 7:
+Realizar un programa que pida al usuario dos números y una letra: “i” ó “p”. El programa presentará los 
+números pares (si se pulsó la “p”) ó impares (si se pulsó la “i”) que hay desde el primer número al segundo que 
+introdujo el usuario. Si se pulsa alguna tecla distinta de “p” ó “i”, el programa no imprime ningún número.
+
+*/
+
+using System;
+
+namespace Problema_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroUno;
+            int numeroDos;
+            char opcion;
+
+            Console.Write("Ingrese un número: ");
+            numeroUno = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Ingrese un número: ");
+            numeroDos = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Ingrese 'p' para presentar los números pares o 'i' para números impares: ");
+            opcion = Console.ReadKey().KeyChar;
+            Console.WriteLine("\n");
+
+            switch (opcion){
+                case 'p' :
+                    for (int i = numeroUno; i <= numeroDos; i++)
+                    {
+                        if(i % 2 == 0){
+                             Console.Write(i + " ");
+                        }
+                    }
+                    break;
+                case 'i':
+                    for (int i = numeroUno; i <= numeroDos; i++)
+                    {
+                        if(i % 2 != 0){
+                             Console.Write(i + " ");
+                        }
+                    }
+                    break;
+                default:
+                    Console.WriteLine("");
+                    break;
+                    
+
+                
+
+            }
+        }
+    }
+}
