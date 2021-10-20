57301e29cd8a565ddd38e32d512f4e96c0e216cc
Paula Leiva <88547780+PaulaLeiva@users.noreply.github.com>
2021-10-19 21:55:30 -0500

    Add files via upload

---
+++ Corte 3/Actividad_7/Ejercicio_10/Program.cs
@@ -0,0 +1,44 @@
+﻿/*
+Create by: John Fredy Marciales Morales.
+Group: 239-2A
+Shift: day
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+
+namespace C_8octubre
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Queue<float> C = new Queue<float>();
+            Queue<float> nuevaCola = new Queue<float>();
+            int finalizar = 1;
+            int CSize = 0;
+            while (finalizar == 1)
+            {
+                Console.Write("Digite un número flotante: ");
+                C.Enqueue(Convert.ToSingle(Console.ReadLine()));
+                CSize++;
+                Console.WriteLine("Para ingresar un nuevo número, digite 1. De lo contrario, ingrese cualquier otro número.");
+                finalizar = Convert.ToInt32(Console.ReadLine());
+            }
+            float[] dataCola = new float[CSize];
+            dataCola = C.ToArray();
+            for (int i = CSize-1; i >= 0; i--)
+            {
+                nuevaCola.Enqueue(Convert.ToSingle(dataCola[i]));
+                Console.WriteLine(dataCola[i]);
+            }
+        }
+
+    }
+}
---
+++ Corte 3/Actividad_7/Ejercicio_12/Program.cs
@@ -0,0 +1,42 @@
+﻿/*
+Create by: John Fredy Marciales Morales.
+Group: 239-2A
+Shift: day
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> C = new Queue<int>();
+            Queue<int> nuevaCola = new Queue<int>();
+            int finalizar = 1;
+            int CSize = 0;
+            while (finalizar == 1)
+            {
+                Console.Write("Digite un número entero: ");
+                C.Enqueue(Convert.ToInt32(Console.ReadLine()));
+                CSize++;
+                Console.WriteLine("Para ingresar un nuevo número, digite 1. De lo contrario, ingrese cualquier otro número.");
+                finalizar = Convert.ToInt32(Console.ReadLine());
+            }
+            int[] data = new int[CSize];
+            data = C.ToArray();
+            for (int i = 0; i < CSize; i++)
+            {
+                nuevaCola.Enqueue(Convert.ToInt32(data[i]));
+                Console.WriteLine(data[i]);
+                C.Dequeue();
+            }
+        }
+
+    }
+
